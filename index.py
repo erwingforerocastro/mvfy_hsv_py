b10adb4a94f3dcea40a861ff3666f8959b53e6b1
erwingforerocastro <erwingforerocastro@gmail.com>
2022-06-18 22:30:37 -0500

    solve problems with pipfile

--- index.py
+++ index.py
@@ -10,25 +10,22 @@ if __name__ == "__main__":
         const.ALLOWED_FEATURES["GENDER"]
     ]
     visual = VisualKnowledge(
-        type_service = const.TYPE_SERVICE["LOCAL"],
-        db_properties = "mongodb://localhost:27017/",
-        db_name = "mvfy",
-        max_descriptor_distance = 0.7,
-        min_date_knowledge = const.DAYS(7),
+        type_service=const.TYPE_SERVICE["LOCAL"],
+        db_properties="mongodb://localhost:27017/",
+        db_name="mvfy",
+        max_descriptor_distance=0.7,
+        min_date_knowledge=const.DAYS(7),
         type_system=const.TYPE_SYSTEM["OPTIMIZED"],
-        features = features,
-        title = "mvfy_1"
+        features=features,
+        title="mvfy_1"
     )
 
     visual.set_conf(
         detector=FaceRecognition(),
-        receiver=Receiver.ip_cam_receiver(ip_cam="rtsp://mvfysystem:mvfysystem@192.168.1.3:8080/h264_ulaw.sdp"),
+        receiver=Receiver.ip_cam_receiver(
+            ip_cam="rtsp://mvfysystem:mvfysystem@192.168.1.6:8080/h264_ulaw.sdp"),
         streamer=Streamer.stream_local,
-        display_size = (1080, 720)
+        display_size=(1080, 720)
     )
-    
+
     asyncio.run(visual.start())
-
-
-
-    
