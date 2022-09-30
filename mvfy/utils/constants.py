525a309e41ac133f8b5b4d198a9aba74e8b69a7f
ErwingForeroXpert <erwing.forero@xpertgroup.co>
2022-09-29 21:03:13 -0500

    update readme

--- mvfy/utils/constants.py
+++ mvfy/utils/constants.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict, Tuple
 from .feature_flags import ENVIROMENT
 
 BASE_PROJECT = os.path.abspath(
@@ -11,40 +12,32 @@ MODELS_URL = os.path.join(BASE_PROJECT, 'src/mvfy/models')
 CONFIG_URL = os.path.join(BASE_PROJECT, 'config')
 PORT = 3000 if (v:=os.getenv("PORT")) is None else v
 
-COLLECTIONS = {
+#static
+COLLECTIONS: Dict[str, str] = {
     "USERS": "users",
     "SYSTEMS": "systems"
 }
 
-#static
-HTML_STREAMER = {
-    "URL": os.path.join(BASE_PROJECT, '/public/streamer.html'),
-    "URL_TEMP": os.path.join(BASE_PROJECT, '/public/temp/_streamer.html'),
-    "PROTOCOL_REPLACE": '<<<PROTOCOL>>>',
-    "HOST_REPLACE": '<<<HOST>>>',
-    "EMIT_REPLACE": '<<<EMIT>>>',
-}
-
 #system
-ALLOWED_FEATURES = {
+ALLOWED_FEATURES: Dict[str, str] = {
     "AGE": "age",
     "GENDER": "gender",
     "RACE": "race",
     "EMOTION": "emotion"
 }
-TYPE_SYSTEM = {
+TYPE_SYSTEM: Dict[str, str] = {
     "OPTIMIZED": "optimized",
     "PRECISE": "precise"
 }
-TYPE_SERVICE = {
+TYPE_SERVICE: Dict[str, str] = {
     "REMOTE": "remote",
     "LOCAL": "local"
 }
-ACTION = {
+ACTION: Dict[str, str] = {
     "INIT_SYSTEM": "INIT_SYSTEM",
     "SET_DETECTION": "SET_DETECTION",
 }
-REQUEST = {
+REQUEST: Dict[str, str] = {
     "ERROR": "ERROR",
     "GET_MODEL_FEATURES": "GET_MODEL_FEATURES",
     "GET_INITIALIZED_SYSTEM": "GET_INITIALIZED_SYSTEM",
@@ -52,27 +45,36 @@ REQUEST = {
     "LOCAL_IMAGE_SEND": "LOCAL_IMAGE_SEND"
 }
 
-# time
-DATE_FORMAT = "DD/MM/YYYY"
+HTML_STREAMER: Dict[str, str] = {
+    "URL": os.path.join(BASE_PROJECT, '/public/streamer.html'),
+    "URL_TEMP": os.path.join(BASE_PROJECT, '/public/temp/_streamer.html'),
+    "PROTOCOL_REPLACE": '<<<PROTOCOL>>>',
+    "HOST_REPLACE": '<<<HOST>>>',
+    "EMIT_REPLACE": '<<<EMIT>>>',
+}
 
-def DAYS (quantity: int):
+# time
+DATE_FORMAT: str = "DD/MM/YYYY"
+
+        
+def DAYS (quantity: int) -> Tuple[int, str]:
     quantity = int(quantity)
-    if isinstance(quantity, (int, float)):
+    if isinstance(quantity, int):
         return (quantity, "days")
     else:
         raise ValueError("type of the quantity days is invalid")
     
 
-def WEEKS (quantity: int) :
+def WEEKS (quantity: int) -> Tuple[int, str]:
     quantity = int(quantity)
-    if isinstance(quantity, (int, float)):
+    if isinstance(quantity, int):
         return (quantity, "weeks")
     else:
         raise ValueError("type of the quantity weeks is invalid")
     
-def MONTHS  (quantity: int) :
+def MONTHS  (quantity: int) -> Tuple[int, str]:
     quantity = int(quantity)
-    if isinstance(quantity, (int, float)):
+    if isinstance(quantity, int):
         return (quantity, "months")
     else: 
         raise ValueError("type of the quantity months is invalid")
